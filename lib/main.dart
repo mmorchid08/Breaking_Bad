09ef9045c00069c4ec0fdc08ceae746d5f2fffbb
Mohamed-amine Morchid <mmorchid@e2r3p6.1337.ma>
2021-12-13 13:36:58 +0100

    init_project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BreakingBadApp());
+}
+
+class BreakingBadApp extends StatelessWidget {
+  const BreakingBadApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: ,
+    );
+  }
+}
+
