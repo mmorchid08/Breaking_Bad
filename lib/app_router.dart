e2a5dadf22ca57814b3b8667180d01dcd9590f75
Mohamed-amine Morchid <mmorchid@e2r3p6.1337.ma>
2021-12-14 15:35:20 +0100

    create Loaded state, implemt bloc provider in Approute CharactersScreen

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_one/business_logic/cubit/characters_cubit.dart';
 import 'package:project_one/constants/strings.dart';
+import 'package:project_one/data/repository/characters_repository.dart';
+import 'package:project_one/data/web_services/characters_web_services.dart';
 import 'package:project_one/presentation/screens/characters_screen.dart';
 import 'package:project_one/presentation/screens/charaters_screnn_details.dart';
 
 class AppRouter {
+  late CharactersRepository charactersRepository;
+  late CharactersCubit charactersCubit;
+  AppRouter() {
+    charactersRepository = CharactersRepository(CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case charactersScreen:
-        return MaterialPageRoute(builder: (_) => CharactersScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (BuildContext context) => charactersCubit,
+            child: CharactersScreen(),
+          ),
+        );
       case characterDetailsScreen:
         return MaterialPageRoute(builder: (_) => CharatersDetailsScreen());
     }
--- lib/business_logic/cubit/characters_cubit.dart
+++ lib/business_logic/cubit/characters_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:project_one/data/models/character_model.dart';
+import 'package:project_one/data/repository/characters_repository.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  final CharactersRepository charactersRepository;
+  late List<Character> characters;
+  CharactersCubit(this.charactersRepository) : super(CharactersInitial());
+
+  List<Character> getAllCharacters() {
+    charactersRepository.getAllCharaters().then((characters) {
+      emit(CharactersLoaded(characters));
+      this.characters = characters; 
+    });
+    return characters;
+  }
 }
